8a08406bda24261e6a2bd541633298981d3fef1e
PEDRO HENRIQUE HOFFMANN <pedro_h_hoffmann@estudante.sesisenai.org.br>
2024-07-04 09:39:18 -0300

    prova

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+    return (
+        <header className="cabecalho">
+            <img className= 'senai' src='./senailogo.png'/>
+            <h1>Pedro Henrique Hoffmann - Turma: 3B</h1>
+        </header>
+    );
+}
